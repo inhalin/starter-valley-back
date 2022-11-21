1b10d3df34bb83d6dca2d4fa5aeae9c174f7b3f9
inhalin <inhalin.kim@gmail.com>
2022-11-21 15:37:13 +0900

    chore: 프로젝트 생성함 #2

---
+++ src/main/java/startervalley/backend/StarterValleyBackApplication.java
@@ -0,0 +1,13 @@
+package startervalley.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterValleyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterValleyBackApplication.class, args);
+    }
+
+}
