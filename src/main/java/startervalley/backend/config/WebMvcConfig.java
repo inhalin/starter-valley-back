66d14e2be0422b6a11c2f97d8f17efdc27661a3e
Seokju Hong <iamseokjuhong@gmail.com>
2022-11-24 14:56:43 +0900

    feat(공통): CORS 허용 설정 #7
    리액트에서 스프링 통신할 수 있도록 CORS를 허용했습니다.

---
+++ src/main/java/startervalley/backend/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package startervalley.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
