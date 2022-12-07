8690ab56ab677e5e738db87a81bfb6c96c3e9067
inhalin <inhalin.kim@gmail.com>
2022-12-07 16:06:42 +0900

    feat(로그인): 로그인 부분 validation 적용함 #6

--- src/main/java/startervalley/backend/controller/AuthController.java
+++ src/main/java/startervalley/backend/controller/AuthController.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import startervalley.backend.dto.auth.AuthRequest;
-import startervalley.backend.dto.auth.AuthResponse;
-import startervalley.backend.dto.auth.JwtTokenDto;
-import startervalley.backend.dto.auth.SignupRequest;
+import startervalley.backend.dto.auth.*;
 import startervalley.backend.security.auth.CustomUserDetails;
 import startervalley.backend.service.auth.AuthService;
 import startervalley.backend.service.auth.GithubAuthService;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RestController
@@ -31,12 +30,14 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<JwtTokenDto> signup(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody SignupRequest request) {
+    public ResponseEntity<JwtTokenDto> signup(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @Valid @RequestBody SignupRequest request) {
         return ResponseEntity.ok(authService.signup(userDetails.getAttributes(), request));
     }
 
     @PostMapping("/signup/validate")
-    public ResponseEntity<Void> validateCode(@RequestBody SignupRequest request) {
+    public ResponseEntity<Void> validateCode(@Valid @RequestBody CodeValidationRequest request) {
         authService.validateGenerationCode(request.getCode(), request.getGenerationId());
         return ResponseEntity.ok(null);
     }
