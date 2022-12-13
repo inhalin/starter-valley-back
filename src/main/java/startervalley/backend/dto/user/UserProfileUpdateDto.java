ac9e3cca61d40ab93b6173684d2ecfe36184320f
inhalin <inhalin.kim@gmail.com>
2022-12-13 09:37:40 +0900

    chore(수강생정보): 한줄소개 필수 기입하도록 validation 추가함 #15

--- src/main/java/startervalley/backend/dto/user/UserProfileUpdateDto.java
+++ src/main/java/startervalley/backend/dto/user/UserProfileUpdateDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Getter
@@ -17,7 +18,9 @@ public class UserProfileUpdateDto {
     @Pattern(regexp = "([EI][SN][TF][JP])*$", flags = {Pattern.Flag.CASE_INSENSITIVE}, message = "정확한 MBTI를 적어주세요.")
     private String mbti;
 
+    @NotNull(message = "한줄소개는 필수입니다.")
     private String intro;
+
     private String likes;
     private String dislikes;
     private String interests;
