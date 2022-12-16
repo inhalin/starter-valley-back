6e71282066294dced199ade8b499094e96983b75
inhalin <inhalin.kim@gmail.com>
2022-12-16 14:05:36 +0900

    feat(런치버스): 버스 삭제, 과거 운행 버스 목록 #23

--- src/main/java/startervalley/backend/controller/LunchbusController.java
+++ src/main/java/startervalley/backend/controller/LunchbusController.java
@@ -6,8 +6,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import startervalley.backend.dto.lunchbus.LunchbusDto;
 import startervalley.backend.dto.lunchbus.LunchbusInsertRequest;
-import startervalley.backend.dto.lunchbus.LunchbusResponse;
 import startervalley.backend.dto.lunchbus.LunchbusSimpleDto;
+import startervalley.backend.dto.common.BasicResponse;
 import startervalley.backend.security.auth.CustomUserDetails;
 import startervalley.backend.service.LunchbusService;
 
@@ -22,19 +22,31 @@ public class LunchbusController {
     private final LunchbusService lunchbusService;
 
     @PostMapping
-    public ResponseEntity<LunchbusResponse> create(
+    public ResponseEntity<BasicResponse> create(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody LunchbusInsertRequest request) {
         return ResponseEntity.ok(lunchbusService.saveLunchbus(userDetails.getId(), request));
     }
 
     @GetMapping
-    public ResponseEntity<List<LunchbusSimpleDto>> list(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        return ResponseEntity.ok(lunchbusService.findAllActive(userDetails.getUser().getGeneration().getId()));
+    public ResponseEntity<List<LunchbusSimpleDto>> list() {
+        return ResponseEntity.ok(lunchbusService.findAllByActive(true));
+    }
+
+    @GetMapping("/past")
+    public ResponseEntity<List<LunchbusSimpleDto>> listPast() {
+        return ResponseEntity.ok(lunchbusService.findAllByActive(false));
     }
 
     @GetMapping("/{busId}")
     public ResponseEntity<LunchbusDto> show(@PathVariable Long busId) {
-        return ResponseEntity.ok(lunchbusService.findOneById(busId));
+        return ResponseEntity.ok(lunchbusService.findLunchbus(busId));
+    }
+
+    @DeleteMapping("/{busId}")
+    public ResponseEntity<BasicResponse> delete(@AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long busId) {
+        lunchbusService.validateDriver(userDetails.getId(), busId);
+
+        return ResponseEntity.ok(lunchbusService.deleteLunchbus(busId));
     }
 }
