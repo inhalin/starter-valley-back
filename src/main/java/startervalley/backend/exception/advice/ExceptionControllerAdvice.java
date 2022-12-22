6640a8f93faf671c397cad7d0e967e93d12a6229
inhalin <inhalin.kim@gmail.com>
2022-12-22 15:19:50 +0900

    fix: 잘못 머지한 부분 수정

--- src/main/java/startervalley/backend/exception/advice/ExceptionControllerAdvice.java
+++ src/main/java/startervalley/backend/exception/advice/ExceptionControllerAdvice.java
@@ -49,12 +49,6 @@ public class ExceptionControllerAdvice {
         return new ErrorResult(e.getMessage());
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(StoreImageUploadFailedException.class)
-    public ErrorResult handleStoreImageUploadFailedException(StoreImageUploadFailedException e) {
-        return new ErrorResult(e.getMessage());
-    }
-
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(TokenNotValidException.class)
     public ResponseEntity<ErrorResult> handleTokenValidationException(TokenNotValidException e, WebRequest request) {
