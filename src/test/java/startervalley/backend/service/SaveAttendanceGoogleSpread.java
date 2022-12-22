ad214fca7d38bf0bc8ae25bf73a085d8c1a1709e
inhalin <inhalin.kim@gmail.com>
2022-12-22 15:02:40 +0900

    chore: dev 브랜치 수동 merge #23

--- src/test/java/startervalley/backend/service/SaveAttendanceGoogleSpread.java
+++ src/test/java/startervalley/backend/service/SaveAttendanceGoogleSpread.java
@@ -1,13 +1,13 @@
 package startervalley.backend.service;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;\
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;\
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.annotation.Transactional;\
+import org.springframework.transaction.annotation.Transactional;
 import startervalley.backend.entity.User;
 import startervalley.backend.repository.AttendanceRepository;
 import startervalley.backend.repository.UserRepository;
