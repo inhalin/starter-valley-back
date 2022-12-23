f9a2cb72ace8e620d9c27617338c5f2fbdce932f
inhalin <inhalin.kim@gmail.com>
2022-12-23 17:37:45 +0900

    feat(런치버스): 버스 상세화면에 승객 목록 추가함 #48

--- src/main/java/startervalley/backend/dto/lunchbus/LunchbusUserDto.java
+++ src/main/java/startervalley/backend/dto/lunchbus/LunchbusUserDto.java
@@ -1,11 +1,16 @@
 package startervalley.backend.dto.lunchbus;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class LunchbusUserDto {
+    private Long userId;
     private String imageUrl;
     private String name;
 }
