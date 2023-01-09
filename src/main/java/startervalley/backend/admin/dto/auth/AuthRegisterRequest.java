2dc2413500a23a4fe37935568e8672c7bc972a17
inhalin <inhalin.kim@gmail.com>
2023-01-09 11:44:58 +0900

    feat(백오피스): 오타 수정, 특수문자 종류 추가 #47

--- src/main/java/startervalley/backend/admin/dto/auth/AuthRegisterRequest.java
+++ src/main/java/startervalley/backend/admin/dto/auth/AuthRegisterRequest.java
@@ -11,12 +11,12 @@ import javax.validation.constraints.Size;
 public class AuthRegisterRequest {
 
     @NotEmpty(message = "아이디 입력은 필수입니다.")
-    @Size(min = 4, message = "아이디는 4자 이상어야 합니다.")
+    @Size(min = 4, message = "아이디는 4자 이상이어야 합니다.")
     private String username;
 
     @NotEmpty(message = "비밀번호 입력은 필수입니다.")
     @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d!@#$%^&*/]{8,}$", message = "비밀번호는 영문자, 숫자, 특수문자를 1개 이상 포함하여야 합니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*/?])[A-Za-z\\d~!@#$%^&*/?]{8,}$", message = "비밀번호는 영문자, 숫자, 특수문자를 1개 이상 포함하여야 합니다.")
     private String password;
 
     @NotEmpty(message = "이름 입력은 필수입니다.")
