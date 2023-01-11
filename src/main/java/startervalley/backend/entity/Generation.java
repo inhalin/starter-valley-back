b9dc44566cd6fae36ad70e68789755ab0415fb32
Kimin <inhalin.kim@gmail.com>
2023-01-11 18:13:34 +0900

    Revert "Revert "#102 기수 생성, 전체 조회, 단건 조회, 수정, 삭제""

--- src/main/java/startervalley/backend/entity/Generation.java
+++ src/main/java/startervalley/backend/entity/Generation.java
@@ -1,27 +1,26 @@
 package startervalley.backend.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static javax.persistence.GenerationType.*;
-
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
+@DynamicUpdate
 public class Generation extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "generation_id")
+    @Column(name = "generation_id", unique = true, nullable = false)
     private Long id;
 
     @Column(updatable = false)
@@ -34,6 +33,38 @@ public class Generation extends BaseTimeEntity {
     private LocalDate courseStartDate;
     private LocalDate courseEndDate;
 
+    private String recruitUrl;
+    private String submitUrl;
+    private String submitResultUrl;
+
     @OneToMany(mappedBy = "generation")
-    private List<User> users;
+    private List<User> users = new ArrayList<>();
+
+    @Builder
+    public Generation(Long id, String code, String description, String location, Double latitude, Double longitude, LocalDate courseStartDate, LocalDate courseEndDate, String recruitUrl, String submitUrl, String submitResultUrl, List<User> users) {
+        this.id = id;
+        this.code = code;
+        this.description = description;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.courseStartDate = courseStartDate;
+        this.courseEndDate = courseEndDate;
+        this.recruitUrl = recruitUrl;
+        this.submitUrl = submitUrl;
+        this.submitResultUrl = submitResultUrl;
+        this.users = users;
+    }
+
+    public void update(LocalDate courseStartDate, LocalDate courseEndDate, String description, String location, Double latitude, Double longitude, String recruitUrl, String submitUrl, String submitResultUrl) {
+        this.courseStartDate = courseStartDate;
+        this.courseEndDate = courseEndDate;
+        this.description = description;
+        this.location = location;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.recruitUrl = recruitUrl;
+        this.submitUrl = submitUrl;
+        this.submitResultUrl = submitResultUrl;
+    }
 }
--- src/main/java/startervalley/backend/repository/generation/GenerationRepository.java
+++ src/main/java/startervalley/backend/repository/generation/GenerationRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import startervalley.backend.entity.Generation;
 
 public interface GenerationRepository extends JpaRepository<Generation, Long>, GenerationRepositoryCustom {
+
+    boolean existsById(Long id);
 }
