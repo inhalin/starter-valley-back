aab1ad3fe763d9fb6a72181dd2aec7d65ee8b6ff
inhalin <inhalin.kim@gmail.com>
2023-01-16 14:55:58 +0900

    chore: 기수별 교육 시작일/종료일 필드 추가 #120

--- src/main/java/startervalley/backend/admin/dto/user/UserResponse.java
+++ src/main/java/startervalley/backend/admin/dto/user/UserResponse.java
@@ -1,10 +1,12 @@
 package startervalley.backend.admin.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import startervalley.backend.entity.Generation;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Getter
@@ -12,11 +14,20 @@ import java.util.List;
 @Builder
 public class UserResponse {
     private Long generationId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
     private List<UserDto> users;
 
     public static UserResponse mapToResponse(Generation generation, List<UserDto> users) {
         return UserResponse.builder()
                 .generationId(generation.getId())
+                .startDate(generation.getCourseStartDate())
+                .endDate(generation.getCourseEndDate())
                 .users(users)
                 .build();
     }
