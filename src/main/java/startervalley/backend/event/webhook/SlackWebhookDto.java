905120a26144dedb3ae8f13c7f33fdd0cdac1ca3
inhalin <inhalin.kim@gmail.com>
2023-01-17 15:04:29 +0900

    chore: 슬랙 알림 링크 추가 #117

--- src/main/java/startervalley/backend/event/webhook/SlackWebhookDto.java
+++ src/main/java/startervalley/backend/event/webhook/SlackWebhookDto.java
@@ -21,26 +21,25 @@ public class SlackWebhookDto {
 
     private static String objToText(Object o) {
         StringBuilder sb = new StringBuilder();
+        final String releaseUri = "https://starter-valley.site";
 
         if (o.getClass() == Lunchbus.class) {
             Lunchbus bus = (Lunchbus) o;
 
-            sb.append("🚌 런치버스가 승객을 모집합니다.\n\n")
+            return sb.append("🚌 런치버스가 승객을 모집합니다.\n\n")
                     .append("제목: ").append(bus.getTitle()).append("\n")
                     .append("운전자: ").append(bus.getDriver().getName()).append("\n")
-                    .append("모집인원: ").append(bus.getOccupancy()).append("\n");
-
-            if (bus.getClosedDate() != null) {
-                sb.append("출발시간: ").append(bus.getClosedDate().format(DateTimeFormatter.ofPattern("MM.dd. E요일 HH시 mm분")));
-            }
-
-            return sb.toString();
+                    .append("모집인원: ").append(bus.getOccupancy()).append("\n")
+                    .append("출발시간: ").append(bus.getClosedDate() != null ? bus.getClosedDate().format(DateTimeFormatter.ofPattern("MM.dd. E요일 HH시 mm분")) : "미정").append("\n\n")
+                    .append("바로가기 👉 ").append(releaseUri).append("/lunchbus/").append(bus.getId())
+                    .toString();
         } else if (o.getClass() == Notice.class) {
             Notice notice = (Notice) o;
 
             return sb.append("🔔 공지사항이 등록되었습니다.\n\n")
                     .append("제목: ").append(notice.getTitle()).append("\n")
-                    .append("작성자: ").append(notice.getAdminUser().getName()).append("\n")
+                    .append("작성자: ").append(notice.getAdminUser().getName()).append("\n\n")
+                    .append("바로가기 👉 ").append(releaseUri).append("/notice/").append(notice.getId())
                     .toString();
         }
 
